4d4856e3628e09fc851b592d10345954e1ea36c6
Aditi <adtsolanki22@gmail.com>
2022-12-23 20:00:57 +0530

    Auth Controller and Services

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,19 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 
-@Controller()
+@Controller('auth')
 export class AuthController {
+    //nestjs instantiate auth service ans pass it
+    constructor(private authService: AuthService) {}
 
+    // post --> auth/signup
+    @Post('signup')
+    signup() {
+        return this.authService.signup();
+    }
+
+    @Post('signin')
+    signin() {
+        return this.authService.signin();
+    }
 }
